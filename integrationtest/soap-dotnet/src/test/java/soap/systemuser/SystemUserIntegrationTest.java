cae31bbb95afafecaf2614e90262a3f38442ccbe
Simon Hem Pedersen <sihpe@nets.eu>
2021-02-08 17:11:00 +0100

    NLRFIM-41 trust dotnet ssl certificate

--- integrationtest/soap-dotnet/src/test/java/soap/systemuser/SystemUserIntegrationTest.java
+++ integrationtest/soap-dotnet/src/test/java/soap/systemuser/SystemUserIntegrationTest.java
@@ -1,17 +1,13 @@
 package soap.systemuser;
 
-import org.example.contract.helloworld.HelloWorldPortType;
-import org.example.contract.helloworld.HelloWorldService;
 import org.junit.Test;
 import org.tempuri.HelloWorld;
 import org.tempuri.IHelloWorld;
 
-import static org.junit.Assert.assertEquals;
-
 public class SystemUserIntegrationTest {
 
     @Test
-    public void testSystemUserScenarioJavaWscToDotNetWsp() {
+    public void testSystemUserScenarioJavaWscToDotNetWsp_None() {
         //Arrange
         System.setProperty("javax.net.ssl.trustStore", "src/test/resources/ssl-trust.jks");
         System.setProperty("javax.net.ssl.trustStorePassword", "Test1234");
@@ -19,7 +15,17 @@ public class SystemUserIntegrationTest {
         IHelloWorld port = service.getSoapBindingIHelloWorld();
 
         String response = port.helloNone("John");
-
-        //TODO more tests
     }
+
+    @Test
+    public void testSystemUserScenarioJavaWscToDotNetWsp_Sign() {
+        //Arrange
+        System.setProperty("javax.net.ssl.trustStore", "src/test/resources/ssl-trust.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "Test1234");
+        HelloWorld service = new HelloWorld();
+        IHelloWorld port = service.getSoapBindingIHelloWorld();
+
+        String response = port.helloSign("John");
+    }
+
 }
