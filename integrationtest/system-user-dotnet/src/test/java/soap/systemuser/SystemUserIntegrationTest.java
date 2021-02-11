7102044a0dd5f337d6da1db8c670a93026de7e8b
Simon Hem Pedersen <sihpe@nets.eu>
2021-02-11 13:22:56 +0100

    NLRFIM-35 more test-cases

--- integrationtest/system-user-dotnet/src/test/java/soap/systemuser/SystemUserIntegrationTest.java
+++ integrationtest/system-user-dotnet/src/test/java/soap/systemuser/SystemUserIntegrationTest.java
@@ -1,8 +1,12 @@
 package soap.systemuser;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.tempuri.HelloWorld;
 import org.tempuri.IHelloWorld;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SystemUserIntegrationTest {
 
@@ -15,6 +19,8 @@ public class SystemUserIntegrationTest {
         IHelloWorld port = service.getSoapBindingIHelloWorld();
 
         String response = port.helloNone("John");
+
+        assertTrue(response.contains("Hello None John"));
     }
 
     @Test
@@ -26,6 +32,22 @@ public class SystemUserIntegrationTest {
         IHelloWorld port = service.getSoapBindingIHelloWorld();
 
         String response = port.helloSign("John");
+
+        assertTrue(response.contains("Hello Sign John"));
+    }
+
+    @Test
+    @Ignore("encryption-part not working")
+    public void testSystemUserScenarioJavaWscToDotNetWsp_EncryptAndSign() {
+        //Arrange
+        System.setProperty("javax.net.ssl.trustStore", "src/test/resources/ssl-trust.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "Test1234");
+        HelloWorld service = new HelloWorld();
+        IHelloWorld port = service.getSoapBindingIHelloWorld();
+
+        String response = port.helloEncryptAndSign("John");
+
+        assertTrue(response.contains("Hello Encrypt and Sign John"));
     }
 
 }
